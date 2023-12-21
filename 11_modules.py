3f1544d8abf082505c1475aa6f199df1d552e652
cesarbravoalamo <cesarbravoalamo@gmail.com>
2023-12-21 18:22:47 +0100

    functions

---
+++ 11_modules.py
@@ -0,0 +1,15 @@
+
+
+from math import pi
+import math
+
+
+print(math.pi)
+print(math.cos(90))
+print(math.pow(2, 12))
+
+print("--------------------------------")
+
+print(pi)
+
+print("--------------------------------")
